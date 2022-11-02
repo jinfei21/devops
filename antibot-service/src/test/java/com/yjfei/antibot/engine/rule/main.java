e081ad43b1d6e976f2b68d48153dd446fc220816
finix <jinfei21@users.noreply.github.com>
2022-11-02 13:56:36 +0800

    Update main.java

--- antibot-service/src/test/java/com/yjfei/antibot/engine/rule/main.java
+++ antibot-service/src/test/java/com/yjfei/antibot/engine/rule/main.java
@@ -25,8 +25,26 @@ public class main {
        },"a");
 
 
-       System.out.println(str);
         System.out.println(str);
+        System.out.println(str);
+        
+        
+        Map<String, String> map = objectMapper.convertValue(request, new TypeReference<Map<String,String>>(){});
+
+        InputStream deploymentYaml = Util.getInputStreamFromTemplate("template/deployment.yaml", map);
+        InputStream serviceYaml = Util.getInputStreamFromTemplate("template/service.yaml", map);
+        InputStream ingressYaml = Util.getInputStreamFromTemplate("template/ingress.yaml", map);
+        
+        Deployment deployment = client.apps().deployments().load(deploymentYaml).get();
+		Ingress ingress = client.network().ingress().load(ingressYaml).get();
+		io.fabric8.kubernetes.api.model.Service service = client.services().load(serviceYaml).get();
+
+		client.apps().deployments().inNamespace(namespace).createOrReplace(deployment);
+		client.services().inNamespace(namespace).createOrReplace(service);
+		client.network().ingress().inNamespace(namespace).createOrReplace(ingress);
+		client.network().ingress().inNamespace(namespace).createOrReplace(ingressPostfix);
+        
+        
 
     }
 }
